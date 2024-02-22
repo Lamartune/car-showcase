68811ceeeff178bf009f5cc4e9f17ec2195a2936
Lamartune <fatih.tun.2002@gmail.com>
2024-02-22 12:18:45 +0300

    Added images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { CustomButton } from "@/components";
+import Image from "next/image";
 
 const Hero = () => {
   const handleScroll = () => {};
@@ -22,6 +23,11 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+        </div>
       </div>
     </div>
   );
